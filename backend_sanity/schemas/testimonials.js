7a5b2c8193e553ae6cf8e2ab6de69dc1b232e98e
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-02 22:05:43 +0400

    fix typo in testimonials.js

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -2,7 +2,7 @@ export default {
     name:'testimonials',
     title:'Testimonials',
     type: 'document',
-    field: [
+    fields: [
         {
             name:'name',
             title:'Name',
