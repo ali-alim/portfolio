cc8ca6d497c043ae0a691abc9c67691a6516c65c
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-03 18:51:48 +0400

    create index.js and export container components

---
+++ frontend/src/container/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./header.scss"
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
---
+++ frontend/src/container/Testimonial/Testimonial.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./testimonial.scss"
+const Testimonial = () => {
+  return (
+    <div>Testimonial</div>
+  )
+}
+
+export default Testimonial
