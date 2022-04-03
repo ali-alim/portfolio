fe00a6222b2b2672811590fd227e0f668dd91600
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-03 11:07:21 +0400

    import created schemas to schema.js

--- backend_sanity/schemas/schema.js
+++ backend_sanity/schemas/schema.js
@@ -4,6 +4,9 @@ import createSchema from 'part:@sanity/base/schema-creator'
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 import testimonials from './testimonials'
+import about from './testimonials'
+import brands from './testimonials'
+import contact from './testimonials'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -13,6 +16,9 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     /* Your types here! */
-    testimonials
+    testimonials,
+    about,
+    brands,
+    contact
   ]),
 })
