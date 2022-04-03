d66dc544c9ffbfc08623e188f0e6f0ea04fcf66d
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-03 11:42:02 +0400

    create works schema and import to schema.js

---
+++ backend_sanity/schemas/works.js
@@ -0,0 +1,22 @@
+export default {
+    name:'works',
+    title:'Works',
+    type:'document',
+    fields: [
+        {
+            name:'title',
+            title:'Title',
+            type:'string'
+        },
+        {
+            name: 'description',
+            title:'Description',
+            type:'string'
+        },
+        {
+            name: 'projectLink',
+            title:'ProjectLink',
+            type: 'string'
+        },
+    ],
+}
