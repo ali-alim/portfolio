a9e47accd10a5f18c74e359489ed265a459bcd66
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-04 22:03:17 +0400

    create client.js for connecting sanity & react

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -5,7 +5,7 @@ import "./about.scss";
 
 const abouts = [
   {
-    title: "FrontEnd Development",
+    title: "Frontend Development",
     description: "I am good at FrontEnd Development",
     imgUrl: images.about01,
   },
@@ -24,27 +24,43 @@ const abouts = [
     description: "Not so addicted",
     imgUrl: images.about04,
   },
+  {
+    title: "Web Development",
+    description: "My passion as well",
+    imgUrl: images.about04,
+  },
 ];
 
 const About = () => {
   return (
     <>
-      <h2 className="app_head-text">
-        What separates design from art is that design is meant to be...
-        functional!
+      <h2 className="app__head-text">
+      <span>What separates design from </span><span>art</span>
+        <br />
+        is that design{" "}
+        <span>
+          <br />
+          is meant to be...</span> 
+          <br />
+          functional!
+        
       </h2>
       <div className="about__profiles">
         {abouts.map((about, index) => (
-          <motion.div 
-          whileInView={{ opacity: 1 }}
-          whileHover={{scale: 1.1}}
-          transition = {{duration: 0.5, type:'tween'}}
-          className="about__profile-item"
-          key={`about-${index}`}
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.2, type: "tween" }}
+            className="about__profile-item"
+            key={`about.title-${index}`}
           >
             <img src={about.imgUrl} alt={about.title} />
-            <h2 className="app__bold-text" style={{margin: 20}}>{about.title}</h2>
-            <p className="app__p-text" style={{marginTop:10}}>{about.description}</p>
+            <h2 className="app__bold-text" style={{ marginTop: 20 }}>
+              {about.title}
+            </h2>
+            <p className="app__p-text" style={{ marginTop: 10 }}>
+              {about.description}
+            </p>
           </motion.div>
         ))}
       </div>
