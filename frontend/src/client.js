762208ed7f7cc688a400729ec509d84bb7926e69
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-04 22:32:19 +0400

    add sanity token and project id, update client.js

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -1,1 +1,14 @@
 import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = sanityClient({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset:'production',
+    appVersion: '2022-04-04',
+    useCdn: true,
+    token: process.env.SANITY_APP_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source);
