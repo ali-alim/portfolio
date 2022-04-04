d9cd253c6393ba402fcfc1fa6d5ddc1c3bdde030
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-04 15:07:49 +0400

    style Header

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -1,9 +1,36 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+import { images } from '../../constants'
 import "./header.scss"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header flex'>
+      <motion.div
+      whileInView={{x:[-100,0],opacity:[0,1]}}
+      transition={{duration:0.5}}
+      className="header-info"
+      >
+        <div className="header-badge">
+          <div className="badge-cmp flex">
+            <span>👋</span>
+            <div style={{marginLeft:20}}>
+            <p className="p-text">Hello, I am</p>
+            <h1 className="head-text">Ali</h1>
+            </div>
+          </div>
+          <div className="tag-cmp flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Freelancer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      
+    
+
+
+    </div>
   )
 }
 
