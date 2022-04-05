a70efb55723b1a347a9f725b7eacd5d79065541d
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-05 18:19:21 +0400

    export AppWrap

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -1,11 +1,22 @@
 import {NavigationDots, SocialMedia} from "../components"
 
-const AppWrap = () => {
+const AppWrap = (Component, idName, classNames) => function HOC() {
   return (
-    <div>
-      APP WRAP
+    <div id={idName} className={`app__container ${classNames}`}>
+      <SocialMedia />
+
+      <div className="app_wrapper app__flex">
+          <Component />
+
+        <div className="copyright">
+            <p className="p-text">@2022 ALI M.</p>
+            <p className="p-text">All rights reserved</p>
+        </div>
+
+    </div>  
+    <NavigationDots active={idName} />
     </div>
   )
 }
 
-export default AppWrap
+export default AppWrap;
