5b4d112bc6b2481536ffad46415e9f527f2ef441
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-06 18:19:39 +0400

    import necessary stuff in Work component

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import {AiFillEye, AiFillGithub} from 'react-icons/ai'
+import {motion} from 'framer-motion'
+import { AppWrap } from '../../wrapper'
+import { urlFor, client } from '../../client'
 import "./work.scss"
 
 const Work = () => {
