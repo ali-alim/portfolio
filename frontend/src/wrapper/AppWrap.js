317e1ce28db646541a3697ea9382139cf58afb2c
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-06 16:01:58 +0400

    add styles to App.scss

--- frontend/src/wrapper/AppWrap.js
+++ frontend/src/wrapper/AppWrap.js
@@ -6,7 +6,7 @@ const AppWrap = (Component, idName, classNames) =>
       <div id={idName} className={`app__container ${classNames}`}>
         <SocialMedia />
 
-        <div className="app_wrapper app__flex">
+        <div className="app__wrapper app__flex">
           <Component />
 
           <div className="app__copyright">
