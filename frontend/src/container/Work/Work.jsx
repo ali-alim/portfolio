f23b1d0173d1ebf3f685b9b892ebcee7cdb5873f
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-06 18:40:55 +0400

    add hooks to the Work component

--- frontend/src/container/Work/Work.jsx
+++ frontend/src/container/Work/Work.jsx
@@ -6,6 +6,22 @@ import { urlFor, client } from '../../client'
 import "./work.scss"
 
 const Work = () => {
+
+  const [works, setWorks] = useState([]);
+  const [filterWork, setFilterWork] = useState([]);
+  const [activeFilter, setActiveFilter] = useState('All');
+  const [animateCard, setAnimateCard] = useState({y:0, opacity:1});
+
+  useEffect(() => {
+    const query = '*[_type == "works"]';
+
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    })
+  }, []);
+  
+
   return (
     <div>Work</div>
   )
