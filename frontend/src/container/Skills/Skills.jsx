eb06b5ff4d83bbe6aed6a7837f46cd466ab98c72
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-07 16:05:00 +0400

    add useEffect hook

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -5,9 +5,32 @@ import { urlFor, client } from '../../client'
 import { AppWrap } from '../../wrapper'
 import "./skills.scss"
 
+
+
 const Skills = () => {
+
+  useEffect(()=>{
+
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query)
+    .then((data) => setExperience(data));
+
+    client.fetch(skillsQuery)
+    .then((data) => setSkills(data));
+
+  }, [])
+
   return (
-    <div></div>
+    <>
+    <h2 className='app__head-text'>Skills & Experience</h2>
+    <div className="skills-container">
+      <motion.div className='skills-list'>
+
+      </motion.div>
+    </div>
+    </>
   )
 }
 
