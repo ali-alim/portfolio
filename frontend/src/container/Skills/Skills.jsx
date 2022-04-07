27e7945e585c2445246a839feadb8721e59a9a93
ali-alim <ali.mamedov.2016@gmail.com>
2022-04-07 15:53:20 +0400

    import necessary stuff in Skills component

--- frontend/src/container/Skills/Skills.jsx
+++ frontend/src/container/Skills/Skills.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { motion } from 'framer-motion'
+import ReactToolTip from 'react-tooltip'
+import { urlFor, client } from '../../client'
+import { AppWrap } from '../../wrapper'
 import "./skills.scss"
 
 const Skills = () => {
   return (
-    <div>Skills</div>
+    <div></div>
   )
 }
 
